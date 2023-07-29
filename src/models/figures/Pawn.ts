a95bd87e8fbae4c72126de7f29f4eb899235cd0a
andrew6613 <abazarov592@gmail.com>
2023-07-29 22:36:19 +0300

    Knight and Pawn can move

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -5,6 +5,8 @@ import blacklogo from "../../assets/black-pawn.png";
 import whitelogo from "../../assets/white-pawn.png";
 
 export class Pawn extends Figure {
+  isFirstStep: boolean = true;
+
   constructor(color: Colors, cell: Cell) {
     super(color, cell);
     this.logo = color === Colors.BLACK ? blacklogo : whitelogo;
@@ -15,6 +17,35 @@ export class Pawn extends Figure {
     if (!super.canMove(target)) {
       return false;
     }
-    return true;
+
+    const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+    const firstStepDirection =
+      this.cell.figure?.color === Colors.BLACK ? 2 : -2;
+    const usualStep = target.y === this.cell.y + direction; // Если это обычный ход пешки, то для хода будет доступна клетка по координате Y+1
+    const firstStep =
+      this.isFirstStep && target.y === this.cell.y + firstStepDirection; // Если у пешки первый ход, то для хода будет доступна клетка по координате Y+2
+
+    if (
+      (usualStep || firstStep) &&
+      target.x === this.cell.x &&
+      this.cell.board.getCell(target.x, target.y).isEmpty()
+    ) {
+      return true;
+    } // проверка на то, куда может пойти пешка
+
+    if (
+      target.y === this.cell.y + direction &&
+      (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) &&
+      this.cell.isEnemy(target)
+    ) {
+      return true;
+    } // атака пешки по диагонали
+
+    return false;
+  }
+
+  moveFigure(target: Cell): void {
+    super.moveFigure(target);
+    this.isFirstStep = false;
   }
 }
